ffff1cd87a41093bc0da11cfed3694c7ae72e1a0
rcao0 <rcao0@example.com>
2026-06-10 08:58:06 -0400

    Astro migration - all pages, components, nav logo, black/white + orange theme, 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sanity from '@sanity/astro';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity()]
+  output: 'static'
 });
